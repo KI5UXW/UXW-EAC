bc53a3fd8e6791c949ae494312131729fd89bff6
James Ervin <109383129+KI5UXW@users.noreply.github.com>
2024-01-31 16:31:24 -0600

    Update EAC.ino
    Added all variables that need to be stored for calculating a Magnetic Loop (also known as a small transmitting loop)'s specifications.

--- EAC.ino
+++ EAC.ino
@@ -2,19 +2,25 @@
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 int currentMenu = 1;
-float freqChoice = 14;
-float antennaLength = 0;
-float powerInput = 100;
 
 //Universal Variables
 float chartOfAWG[30] = {324.9 ,289.3, 257.6, 229.4, 204.3, 181.9, 162, 144.3, 128.5, 114.4, 101.9, 90.7, 80.8, 72, 64.1, 57.1, 50.8, 45.3, 40.3, 35.9, 32, 28.5, 25.3, 22.6, 20.1, 17.9, 15.9, 14.2, 12.6, 11.3, 10}; //Added the AWG 1/10 as the value of 0 here in this array. Data from https://www.lapptannehill.com/resources/technical-information/solid-conductor-awg-dimensions-chart.
 float wireAWG = chartOfAWG[30];
 float inputPower = 100; // Measured in Watts.
-
-//Dipole, OFCD, End-fed Variables
+float freqChoice = 14;
+float antennaLength = 0;
 
 //Magnetic Loop Antenna Variables
-float magloopVoltage = 0; // 
+float loopDiameter = 0;
+float magloopVoltage = 0; // Voltage across the capacitor during TX.
+float radiationResistance = 0; // Measured in Ohms.
+float efficiency = 0; // Measured in decimal: 0.0 = 0% and 1.0 = 100%
+float inductance = 0; // Meaured in Henries.
+float tuningCapacitance = 0; // Measured in Farads.
+float inductiveReactance = 0; // Measured in Ohms.
+float Q = 0; // Quality Factor.
+float bandwidth = 0; // Bandwidth over which the performance is acceptable and measured in Hertz.
+float distributedCapacitance = 0; // Measured in Picofarads.
 
 const int increaseButtonPin = 2;
 const int decreaseButtonPin = 3; 
